700b1cea027dc5d1e509ac6003cf1f2408d2428b
ikram3031 <mdikram.3031@gmail.com>
2025-02-26 01:25:43 +0600

    full one

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
-/* import { Contact } from "@/components/Contact";
-import { Footer } from "@/components/Footer";
-import { Hero } from "@/components/Hero";
-import { KeyMetrics } from "@/components/KeyMetrics";
-import { LogoAnimation } from "@/components/LogoAnimation"; */
-import { Navbar } from "@/components/Navbar";
-/* import { Portfolio } from "@/components/Portfolio";
-import { Services } from "@/components/Services";
-import { Stack } from "@/components/Stack"; */
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <>
-        <Navbar/>
-        {/* <Hero/>
-        <Stack/>
-        <LogoAnimation/>
-        <Portfolio/>
-        <KeyMetrics/>
-        <Services/>
-        <Contact/>
-        <Footer/> */}
-    </>  
-    );
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
+    </main>
+  );
 }
