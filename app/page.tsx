120383d257e6589752a4c24d3f55079689356ffb
ikram3031 <mdikram.3031@gmail.com>
2025-02-26 01:10:58 +0600

    mobile nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+/* import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
+import { Hero } from "@/components/Hero";
+import { KeyMetrics } from "@/components/KeyMetrics";
+import { LogoAnimation } from "@/components/LogoAnimation"; */
 import { Navbar } from "@/components/Navbar";
-import Image from "next/image";
+/* import { Portfolio } from "@/components/Portfolio";
+import { Services } from "@/components/Services";
+import { Stack } from "@/components/Stack"; */
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    </>
-  );
+        <Navbar/>
+        {/* <Hero/>
+        <Stack/>
+        <LogoAnimation/>
+        <Portfolio/>
+        <KeyMetrics/>
+        <Services/>
+        <Contact/>
+        <Footer/> */}
+    </>  
+    );
 }
